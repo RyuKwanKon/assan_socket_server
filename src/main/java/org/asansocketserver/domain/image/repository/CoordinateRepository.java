25e9b777ca4d43f1b70518a71f72ac9cf315ca8e
Jedo0224 <qkrwotjr0@gmail.com>
2024-03-06 10:48:49 +0900

    [feat] 이미지 id 별로 coordinate 값 가져오기

--- src/main/java/org/asansocketserver/domain/image/repository/CoordinateRepository.java
+++ src/main/java/org/asansocketserver/domain/image/repository/CoordinateRepository.java
@@ -5,7 +5,13 @@ import org.asansocketserver.domain.image.entity.Coordinate;
 import org.asansocketserver.domain.image.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 
 public interface CoordinateRepository extends JpaRepository<Coordinate, Long> {
     Coordinate findByImageIdAndPosition(Image image, String position);
+
+    Coordinate findByPosition( String position);
+    List<Coordinate> findAllByImageId(Optional<Image> image);
 }
