a19d02673396c1679a5d4d0e9d0d8694ef2aa98a
Jedo0224 <qkrwotjr0@gmail.com>
2024-03-06 10:49:58 +0900

    [chore] 이미지 조회,수정, 삭제 api 수정

--- src/main/java/org/asansocketserver/domain/image/controller/ImageController.java
+++ src/main/java/org/asansocketserver/domain/image/controller/ImageController.java
@@ -2,16 +2,15 @@ package org.asansocketserver.domain.image.controller;
 
 
 import lombok.RequiredArgsConstructor;
-import org.asansocketserver.domain.image.dto.CoordinateDTO;
-import org.asansocketserver.domain.image.dto.ImageDataDTO;
-import org.asansocketserver.domain.image.dto.ImageResponseDto;
-import org.asansocketserver.domain.image.dto.LabelDataDTO;
+import org.asansocketserver.domain.image.dto.*;
 import org.asansocketserver.domain.image.service.ImageService;
 import org.asansocketserver.domain.position.dto.PositionDTO;
 import org.asansocketserver.global.common.SuccessResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -21,26 +20,43 @@ public class ImageController {
     private final ImageService imageService;
 
     //이미지 조회 api
-    @GetMapping("/getImage")
-    public ResponseEntity<SuccessResponse<?>> getImage() {
-        ImageResponseDto responseDto = imageService.getImage();
+    @GetMapping("/getImage/{id}")
+    public ResponseEntity<SuccessResponse<?>> getImage(@PathVariable Long id) {
+        ImageResponseDto responseDto = imageService.getImage(id);
+        System.out.println("responseDto = " + responseDto);
         return SuccessResponse.ok(responseDto);
     }
 
 
+
+    @GetMapping("/getImageList")
+    public ResponseEntity<SuccessResponse<?>> getImageList()  {
+        ImageListDTO responseDto = imageService.getImageList();
+        System.out.println("responseDto = " + responseDto.getImageIds());
+        return SuccessResponse.ok(responseDto);
+    }
+
+
+
     //이미지 저장 api
     @PostMapping("/saveImage")
-    public ResponseEntity<SuccessResponse<?>> saveImage(@RequestBody ImageDataDTO imageDataDTO) {
-        imageService.saveImage(imageDataDTO.getImageUrl());
-        return SuccessResponse.ok(null);
+    public ResponseEntity<SuccessResponse<?>> saveImage(@RequestParam("imageData") MultipartFile file) throws IOException {
+        Long imageId = imageService.saveImage(file);
+        System.out.println("imageId = " + imageId);
+        return SuccessResponse.ok(imageId);
+    }
+
+    @PostMapping("/nameChange")
+    public ResponseEntity<SuccessResponse<?>> nameChange(@RequestBody ImageIdAndNameDTO imageIdAndNameDTO ) {
+        Long imageId = imageService.nameChange(imageIdAndNameDTO);
+        return SuccessResponse.ok(imageId);
     }
 
 
     //이미지 삭제 api
-    @DeleteMapping("/deleteImage")
-    public ResponseEntity<SuccessResponse<?>> deleteImage(@RequestBody ImageDataDTO imageDataDTO) {
-        Long ImageId = imageDataDTO.getImageId();
-        imageService.deleteImage(ImageId);
+    @DeleteMapping("/deleteImage/{imageId}")
+    public ResponseEntity<SuccessResponse<?>> deleteImage(@PathVariable Long imageId) {
+        imageService.deleteImage(imageId);
         return SuccessResponse.ok(null);
     }
 
@@ -54,9 +70,10 @@ public class ImageController {
 
 
     //이미지내에 설정한 위치들과 해당 좌표 목록을 가져오는 api
-    @GetMapping("/getPositionAndCoordinateList")
-    public ResponseEntity<SuccessResponse<?>> getPositionAndCoordinateList() {
-        List<CoordinateDTO> positionList = imageService.getPositionAndCoordinateList();
+    @GetMapping("/getPositionAndCoordinateList/{id}")
+    public ResponseEntity<SuccessResponse<?>> getPositionAndCoordinateList(@PathVariable Long id) {
+        List<CoordinateDTO> positionList = imageService.getPositionAndCoordinateList(id);
+        System.out.println("positionList = " + positionList);
         return SuccessResponse.ok(positionList);
     }
 
@@ -70,9 +87,9 @@ public class ImageController {
     }
 
     //이미지 내 위치 및 범위 삭제 api
-    @DeleteMapping("/deleteImagePositionAndCoordinates/{coordinateId}")
-    public ResponseEntity<SuccessResponse<?>> deleteImagePositionAndCoordinates(@PathVariable Long coordinateId) {
-        imageService.deleteImagePositionAndCoordinates(coordinateId);
+    @DeleteMapping("/deleteImagePositionAndCoordinates/{positionName}")
+    public ResponseEntity<SuccessResponse<?>> deleteImagePositionAndCoordinates(@PathVariable String positionName) {
+        imageService.deleteImagePositionAndCoordinates(positionName);
         return SuccessResponse.ok(null);
     }
 }
