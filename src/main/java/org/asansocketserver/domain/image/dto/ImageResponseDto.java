ae3eb4314cdd6ec0f7b345cff752fbde24ac1527
Jedo0224 <qkrwotjr0@gmail.com>
2024-03-06 10:50:43 +0900

    [chore] ImageDataDTO, ImageResponseDto에 이미지 Url 항목 추가

--- src/main/java/org/asansocketserver/domain/image/dto/ImageResponseDto.java
+++ src/main/java/org/asansocketserver/domain/image/dto/ImageResponseDto.java
@@ -2,14 +2,20 @@ package org.asansocketserver.domain.image.dto;
 
 import lombok.AccessLevel;
 import lombok.Builder;
+import org.asansocketserver.domain.image.entity.Image;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Builder(access = AccessLevel.PRIVATE)
 public record ImageResponseDto(
-        String url
+        Long imageId,
+        String  imageUrl
 ) {
-    public static ImageResponseDto of(String url) {
+    public static ImageResponseDto of(Long imageId,  String  imageUrl) {
         return ImageResponseDto.builder()
-                .url(url)
+                .imageId(imageId)
+                .imageUrl(imageUrl)
                 .build();
     }
 }
