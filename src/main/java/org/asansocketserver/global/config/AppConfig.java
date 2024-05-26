2f696296d52ac0e57e3f2fc36fbf67155ee3d39d
Jedo0224 <qkrwotjr0@gmail.com>
2024-05-26 10:47:02 +0900

    [fix] taskScheduler Bean 추가

--- src/main/java/org/asansocketserver/global/config/AppConfig.java
+++ src/main/java/org/asansocketserver/global/config/AppConfig.java
@@ -3,9 +3,11 @@ package org.asansocketserver.global.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 import java.util.concurrent.ThreadPoolExecutor;
 
@@ -21,5 +23,15 @@ public class AppConfig {
         executor.setMaxPoolSize(10);
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         return executor;
+    }
+
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(5);
+        scheduler.setThreadNamePrefix("taskScheduler-");
+        scheduler.initialize();
+        return scheduler;
     }
 }
