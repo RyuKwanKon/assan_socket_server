a9053519db7f779286e23c8fe854b71e8a4031bd
Jedo0224 <qkrwotjr0@gmail.com>
2024-06-03 21:53:43 +0900

    [feat] redis에 모니터링 할 워치 id를 저장하는 api 추가

--- src/main/java/org/asansocketserver/domain/sensor/controller/SensorApiController.java
+++ src/main/java/org/asansocketserver/domain/sensor/controller/SensorApiController.java
@@ -1,15 +1,20 @@
 package org.asansocketserver.domain.sensor.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.asansocketserver.domain.position.dto.request.StateDTO;
 import org.asansocketserver.domain.sensor.dto.request.*;
 import org.asansocketserver.domain.sensor.dto.response.*;
 import org.asansocketserver.domain.sensor.service.SensorService;
+import org.asansocketserver.global.common.SuccessResponse;
 import org.asansocketserver.socket.dto.MessageType;
 import org.asansocketserver.socket.dto.SocketBaseResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -18,21 +23,28 @@ import java.util.Map;
 @RestController
 public class SensorApiController {
     private final SensorService sensorService;
-    private final SimpMessageSendingOperations sendingOperations;
+
+
+
+    @PostMapping("/api/sensor/sendState")
+    public ResponseEntity<SuccessResponse<?>> insertState(@RequestBody StateRequestDto stateDTO) {
+        sensorService.sensorSendState(stateDTO);
+        return SuccessResponse.ok("success");
+    }
+
+
 
     @MessageMapping("/accelerometer")
     public void sendAccelerometer(@Header("simpSessionAttributes") Map<String, Object> simpSessionAttributes,
                                   @Payload final AccelerometerRequestDto request) {
-        AccelerometerResponseDto response = sensorService.sendAccelerometer(simpSessionAttributes, request);
-        String destination = "/queue/sensor/" + simpSessionAttributes.get("watchId");
-        sendingOperations.convertAndSend(destination, SocketBaseResponse.of(MessageType.ACCELEROMETER, response));
+        sensorService.sendAccelerometer(simpSessionAttributes, request);
     }
 
     @MessageMapping("/barometer")
     public void sendBarometer(@Header("simpSessionAttributes") Map<String, Object> simpSessionAttributes,
                               @Payload final BarometerRequestDto request) {
         sensorService.sendBarometer(simpSessionAttributes, request);
-        String destination = "/queue/sensor/" + simpSessionAttributes.get("watchId");
+//        String destination = "/queue/sensor/" + simpSessionAttributes.get("watchId");
         // sendingOperations.convertAndSend(destination, SocketBaseResponse.of(MessageType.BAROMETER, response));
     }
 
@@ -40,23 +52,21 @@ public class SensorApiController {
     public void sendGyroscope(@Header("simpSessionAttributes") Map<String, Object> simpSessionAttributes,
                               @Payload final GyroscopeRequestDto request) {
         sensorService.sendGyroscope(simpSessionAttributes, request);
-        String destination = "/queue/sensor/" + simpSessionAttributes.get("watchId");
+//        String destination = "/queue/sensor/" + simpSessionAttributes.get("watchId");
         //sendingOperations.convertAndSend(destination, SocketBaseResponse.of(MessageType.GYROSCOPE, response));
     }
 
     @MessageMapping("/heart-rate")
     public void sendHeartRate(@Header("simpSessionAttributes") Map<String, Object> simpSessionAttributes,
                               @Payload final HeartRateRequestDto request) {
-        HeartRateResponseDto response = sensorService.sendHeartRate(simpSessionAttributes, request);
-        String destination = "/queue/sensor/" + simpSessionAttributes.get("watchId");
-        sendingOperations.convertAndSend(destination, SocketBaseResponse.of(MessageType.HEART_RATE, response));
+        sensorService.sendHeartRate(simpSessionAttributes, request);
     }
 
     @MessageMapping("/light")
     public void sendLight(@Header("simpSessionAttributes") Map<String, Object> simpSessionAttributes,
                           @Payload final LightRequestDto request) {
         sensorService.sendLight(simpSessionAttributes, request);
-        String destination = "/queue/sensor/" + simpSessionAttributes.get("watchId");
+//        String destination = "/queue/sensor/" + simpSessionAttributes.get("watchId");
         // sendingOperations.convertAndSend(destination, SocketBaseResponse.of(MessageType.LIGHT, response));
     }
 }
