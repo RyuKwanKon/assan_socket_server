69a31d79f1220eb0ab1533ae61a917215ee5e532
Jedo0224 <qkrwotjr0@gmail.com>
2024-06-03 21:53:01 +0900

    [feat]  앱에서 redis에 워치 id를 저장했을 경우에만 가속도 센서, 심박수값 보내도록 기능 추가

--- src/main/java/org/asansocketserver/domain/sensor/service/SensorService.java
+++ src/main/java/org/asansocketserver/domain/sensor/service/SensorService.java
@@ -2,6 +2,9 @@ package org.asansocketserver.domain.sensor.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.asansocketserver.domain.position.dto.request.StateDTO;
+import org.asansocketserver.domain.position.entity.PositionState;
+import org.asansocketserver.domain.sensor.SensorSendStateRepository;
 import org.asansocketserver.domain.sensor.dto.request.*;
 import org.asansocketserver.domain.sensor.dto.response.*;
 import org.asansocketserver.domain.sensor.entity.*;
@@ -11,15 +14,26 @@ import org.asansocketserver.domain.sensor.mongorepository.accelerometer.SensorAc
 import org.asansocketserver.domain.sensor.mongorepository.barometer.SensorBarometerRepository;
 import org.asansocketserver.domain.sensor.mongorepository.heartrate.SensorHeartRateRepository;
 import org.asansocketserver.domain.sensor.mongorepository.light.SensorLightRepository;
+import org.asansocketserver.domain.watch.entity.Watch;
+import org.asansocketserver.domain.watch.repository.WatchRepository;
+import org.asansocketserver.global.error.exception.EntityNotFoundException;
+import org.asansocketserver.socket.dto.MessageType;
+import org.asansocketserver.socket.dto.SocketBaseResponse;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 import static org.asansocketserver.domain.sensor.entity.sensorType.Accelerometer.createAccelerometer;
 import static org.asansocketserver.domain.sensor.entity.sensorType.Barometer.createBarometer;
 import static org.asansocketserver.domain.sensor.entity.sensorType.Gyroscope.createGyroscope;
 import static org.asansocketserver.domain.sensor.entity.sensorType.HeartRate.createHeartRate;
 import static org.asansocketserver.domain.sensor.entity.sensorType.Light.createLight;
+import static org.asansocketserver.global.error.ErrorCode.WATCH_UUID_NOT_FOUND;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -31,13 +45,45 @@ public class SensorService {
     private final SensorBarometerRepository sensorBarometerRepository;
     private final SensorHeartRateRepository sensorHeartRateRepository;
     private final SensorLightRepository sensorLightRepository;
+    private final SensorSendStateRepository sensorSendStateRepository;
+    private final WatchRepository watchRepository;
+    private final SimpMessageSendingOperations sendingOperations;
+    private final RedisTemplate<String, Object> redisTemplate;
 
-    public AccelerometerResponseDto sendAccelerometer(Map<String, Object> simpSessionAttributes,
+    private Watch findByWatchOrThrow(Long id) {
+        return watchRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(WATCH_UUID_NOT_FOUND));
+    }
+
+    private SensorSendState findBySensorSendStateOrNull(Long id) {
+        return sensorSendStateRepository.findById(id)
+                .orElse(null);
+    }
+
+
+
+    public void sensorSendState(StateRequestDto stateDTO) {
+        Watch watch = findByWatchOrThrow(stateDTO.watchId());
+        redisTemplate.opsForValue().set("sensorSendState:" +  watch.getId(), watch.getId());
+        redisTemplate.expire("sensorSendState:" + watch.getId(), 3, TimeUnit.MINUTES);
+    }
+
+
+
+
+
+    public void sendAccelerometer(Map<String, Object> simpSessionAttributes,
                                                       AccelerometerRequestDto accelerometerRequestDto) {
         Long watchId = getWatchIdFromSession(simpSessionAttributes);
         Accelerometer createdAccelerometer = createAccelerometer(accelerometerRequestDto);
         createAccelerometerAndSave(watchId, accelerometerRequestDto);
-        return AccelerometerResponseDto.of(createdAccelerometer);
+
+        String destination = "/queue/sensor/" + simpSessionAttributes.get("watchId");
+
+        Object sensorSendState =  redisTemplate.opsForValue().get("sensorSendState:" + watchId );
+
+        if (!Objects.isNull(sensorSendState)) {
+        sendingOperations.convertAndSend(destination, SocketBaseResponse.of(MessageType.ACCELEROMETER, AccelerometerResponseDto.of(createdAccelerometer)));}
     }
 
     public void sendBarometer(Map<String, Object> simpSessionAttributes,
@@ -56,12 +102,18 @@ public class SensorService {
 //        return GyroscopeResponseDto.of(gyroscope);
     }
 
-    public HeartRateResponseDto sendHeartRate(Map<String, Object> simpSessionAttributes,
+    public void sendHeartRate(Map<String, Object> simpSessionAttributes,
                                               HeartRateRequestDto heartRateRequestDto) {
         Long watchId = getWatchIdFromSession(simpSessionAttributes);
         HeartRate heartRate = createHeartRate(heartRateRequestDto);
         createHeartRateAndSave(watchId, heartRateRequestDto);
-        return HeartRateResponseDto.of(heartRate);
+
+        String destination = "/queue/sensor/" + simpSessionAttributes.get("watchId");
+
+        Object sensorSendState = redisTemplate.opsForValue().get("sensorSendState:" + watchId);
+
+        if (!Objects.isNull(sensorSendState)) {
+            sendingOperations.convertAndSend(destination, SocketBaseResponse.of(MessageType.HEART_RATE, HeartRateResponseDto.of(heartRate)));}
     }
 
     public void sendLight(Map<String, Object> simpSessionAttributes,
