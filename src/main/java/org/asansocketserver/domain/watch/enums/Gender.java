d5b3b0461f9e7b80d8b541ad087e940ac7c0816f
Jedo0224 <qkrwotjr0@gmail.com>
2024-08-05 18:25:35 +0900

    [feat] Gender, HighRisk enum 추가

---
+++ src/main/java/org/asansocketserver/domain/watch/enums/Gender.java
@@ -0,0 +1,5 @@
+package org.asansocketserver.domain.watch.enums;
+
+public enum Gender {
+    M, F
+}
---
+++ src/main/java/org/asansocketserver/domain/watch/enums/HighRisk.java
@@ -0,0 +1,5 @@
+package org.asansocketserver.domain.watch.enums;
+
+public enum HighRisk {
+    없음,자살, 자해, 폭력
+}
