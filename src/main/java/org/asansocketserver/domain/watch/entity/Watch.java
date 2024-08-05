5d7dc1241633e717d62d93d3411fa7cba9d04c56
Jedo0224 <qkrwotjr0@gmail.com>
2024-08-05 18:27:01 +0900

    [feat] watch entity에 gender , highRisk ,accessible_area , no_contact_patient 속성 추가 및 updateWatchForWeb 추가

--- src/main/java/org/asansocketserver/domain/watch/entity/Watch.java
+++ src/main/java/org/asansocketserver/domain/watch/entity/Watch.java
@@ -3,6 +3,12 @@ package org.asansocketserver.domain.watch.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.asansocketserver.domain.watch.dto.request.WatchUpdateRequestDto;
+import org.asansocketserver.domain.watch.dto.web.request.WatchUpdateRequestForWebDto;
+import org.asansocketserver.domain.watch.enums.Gender;
+import org.asansocketserver.domain.watch.enums.HighRisk;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
@@ -19,6 +25,24 @@ public class Watch {
     private String device;
     private String name;
     private String host;
+    private int minHR = 60;
+    private int maxHR = 130;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+    @Enumerated(EnumType.STRING)
+    private HighRisk highRisk;
+
+    @ElementCollection
+    @CollectionTable(name = "accessible_area", joinColumns = @JoinColumn(name = "watch_id"))
+    @Column(name = "area_id")
+    private List<Long> accessibleAreaList = new ArrayList<>();
+
+    @ElementCollection
+    @CollectionTable(name = "no_contact_patient", joinColumns = @JoinColumn(name = "watch_id"))
+    @Column(name = "patient_id")
+    private List<Long> noContactPatientList = new ArrayList<>();
+
 
     public static Watch createWatch(String uuid, String device) {
         return Watch.builder()
@@ -26,11 +50,26 @@ public class Watch {
                 .device(device)
                 .name("지정되지않음")
                 .host("지정되지않음")
+                .minHR(60)
+                .maxHR(130)
+                .gender(Gender.M)
+                .highRisk(HighRisk.없음)
+                .accessibleAreaList(new ArrayList<>())
+                .noContactPatientList(new ArrayList<>())
                 .build();
     }
 
     public void updateWatch(WatchUpdateRequestDto requestDto) {
         this.name = requestDto.name();
         this.host = requestDto.host();
+    }
+
+    public void updateWatchForWeb(WatchUpdateRequestForWebDto requestDto) {
+        this.name = requestDto.name();
+        this.host = requestDto.host();
+        this.gender = Gender.valueOf(requestDto.gender());
+        this.highRisk = HighRisk.valueOf(requestDto.highrisk());
+        this.accessibleAreaList = requestDto.accessibleAreaList();
+        this.noContactPatientList = requestDto.noContactPatientList();
     }
 }
