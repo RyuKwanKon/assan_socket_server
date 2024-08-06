e71173ae2ba98d6fa7d1b2b546c2c9fffa1f85df
Jedo0224 <qkrwotjr0@gmail.com>
2024-08-06 10:15:28 +0900

    [feat] ImageResponseDto에 imageName 속성 추가.

--- src/main/java/org/asansocketserver/domain/image/dto/ImageResponseDto.java
+++ src/main/java/org/asansocketserver/domain/image/dto/ImageResponseDto.java
@@ -2,20 +2,19 @@ package org.asansocketserver.domain.image.dto;
 
 import lombok.AccessLevel;
 import lombok.Builder;
-import org.asansocketserver.domain.image.entity.Image;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
 
 @Builder(access = AccessLevel.PRIVATE)
 public record ImageResponseDto(
         Long imageId,
+        String imageName,
         String  imageUrl
 ) {
-    public static ImageResponseDto of(Long imageId,  String  imageUrl) {
+    public static ImageResponseDto of(Long imageId, String imageName ,String  imageUrl) {
         return ImageResponseDto.builder()
                 .imageId(imageId)
+                .imageName(imageName)
                 .imageUrl(imageUrl)
                 .build();
     }
+
 }
