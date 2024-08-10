a106b866b7328926d063dc0e26e794b3573f5f3e
Jedo0224 <qkrwotjr0@gmail.com>
2024-08-10 13:28:37 +0900

    [feat] ImageIDAndNameAndCoordinateDTO 추가

---
+++ src/main/java/org/asansocketserver/domain/image/dto/ImageIDAndNameAndCoordinateDTO.java
@@ -0,0 +1,24 @@
+package org.asansocketserver.domain.image.dto;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+
+import java.util.List;
+
+@Builder(access = AccessLevel.PRIVATE)
+public record ImageIDAndNameAndCoordinateDTO(
+        Long imageId,
+        String imageName,
+        Long coordinateId,
+        String position
+
+) {
+//    public static ImageIDAndNameAndCoordinateDTO of(Long imageId, String imageName, String position) {
+//        return ImageIDAndNameAndCoordinateDTO.builder()
+//                .imageId(imageId)
+//                .imageName(imageName)
+//                .position(position)
+//                .build();
+    }
+
+
