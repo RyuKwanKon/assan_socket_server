7510a4f0610b786cb9d442ca2c3b4e746dc151eb
Jedo0224 <qkrwotjr0@gmail.com>
2024-08-27 16:45:20 +0900

    [feat] CoordinateIDAndPositionDTO 추가 , 환자에게 설정된 구역들에 대한 리스트를 받기 위함.

--- src/main/java/org/asansocketserver/domain/image/dto/CoordinateIDAndPositionDTO.java
+++ src/main/java/org/asansocketserver/domain/image/dto/CoordinateIDAndPositionDTO.java
@@ -1,11 +1,25 @@
 package org.asansocketserver.domain.image.dto;
 
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.asansocketserver.domain.watch.dto.web.response.WatchIdAndNameDto;
 
-@Getter
-@Setter
-public class CoordinateIDAndPositionDTO {
-    private Long coordinateId;
-    private String position;
+import java.util.List;
+
+
+@Builder(access = AccessLevel.PRIVATE)
+public record CoordinateIDAndPositionDTO(
+        Long coordinateId,
+        String position
+
+) {
+    public static CoordinateIDAndPositionDTO of(Long coordinateId, String position) {
+        return CoordinateIDAndPositionDTO.builder()
+                .coordinateId(coordinateId)
+                .position(position)
+                .build();
+    }
+
 }
